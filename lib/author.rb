76b62364f26c51ba6e28322f5751117f2e66b32a
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 16:37:06 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -14,8 +14,8 @@ class Author
     @@post_count +=1
   end
 
-  def add_post_by_title(name)
-    post = Post.new(name)
+  def add_post_by_title(title)
+    post = Post.new(title)
     self.posts << post
     post.author = self
     @@post_count +=1
