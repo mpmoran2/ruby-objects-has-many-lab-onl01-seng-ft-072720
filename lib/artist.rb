d99c508ae171b81e2b8e54c22182d9e0fe822ee0
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 16:38:44 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -7,7 +7,9 @@ class Artist
     @name = name
     @songs = []
   end
-
+ def songs
+    @songs = []
+  end
   def add_song(song)
     self.songs << song
     song.artist = self
@@ -21,9 +23,7 @@ class Artist
     @@song_count +=1
   end
 
-  def songs
-    @songs = []
-  end
+ 
 
   def self.song_count
     @@song_count
