794d11f5d194832a33fe625bd14068e2dcf3eace
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-11 16:45:57 +0000

    Done.

---
+++ lib/author.rb
@@ -0,0 +1,33 @@
+class Author
+  attr_accessor :name
+
+  @@post_count = 0
+
+  def initialize(name)
+    @name = name
+    @posts = []
+  end
+  
+  def posts
+    Post.all.select do |post|
+      post.author == self
+    end
+  end
+  
+  def add_post(post)
+    self.posts << post
+    post.author = self
+    @@post_count +=1
+  end
+
+  def add_post_by_title(title)
+    post = Post.new(title)
+    self.posts << post
+    post.author = self
+    @@post_count +=1
+  end
+
+  def self.post_count
+    Post.all.count
+  end
+end
